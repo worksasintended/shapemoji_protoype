653b70d121eea6e1e6408bcde50a2e6a1ec722b7
Robert Meier <robert.meier@hochschule-stralsund.de>
2020-06-18 15:50:38 +0200

    added maximum travel distance for projectile

--- Shapemoji/Assets/Scripts/HarpoonDraggable.cs
+++ Shapemoji/Assets/Scripts/HarpoonDraggable.cs
@@ -19,17 +19,25 @@ public class HarpoonDraggable : MonoBehaviour
     /// <summary>
     /// moves the connected gameObject 
     /// </summary>
-    /// <param name="speed"></param>
+    /// <param name="speed">speed of the translation</param>
     public void moveDraggable(float speed)
     {
         transform.Translate(Vector3.down * speed);
     }
 
+    /// <summary>
+    /// rotates the objects by the given angle
+    /// </summary>
+    /// <param name="angle">angle to rotate the object</param>
     public void rotate(float angle)
     {
         transform.Rotate(new Vector3(0,0,1),angle);
     }
 
+    /// <summary>
+    /// transfers Item to the inventory
+    /// Not Yet Implemented, just destroys the item
+    /// </summary>
     public void transferToInventory()
     {
         //TODO transfer to Inventory
--- Shapemoji/Assets/Scripts/Projectile.cs
+++ Shapemoji/Assets/Scripts/Projectile.cs
@@ -14,12 +14,20 @@ using UnityEngine;
 /// <param name="rope">
 /// Rope linked to Projectile
 /// </param>
+/// <param name="harpoonZRotation">
+/// configurable angle to turn the collided stone by
+/// </param>
+/// <param name="maxFireDistance">
+/// defines the maximum allowed fire distance
+/// </param>
 public class Projectile : MonoBehaviour
 {
     public bool fired = false;
     public float speed = 400;
     public GameObject rope;
     public float harpoonZRotation = 45;
+    public float maxFireDistance = 1500;
+
     private Vector3 initial;
 
     private GameObject draggable;
@@ -43,11 +51,21 @@ public class Projectile : MonoBehaviour
             fired = false;
             obtain = true;
             maxDistance = getDistanceToCanon();
-            draggable = other.gameObject;
-            if (draggable.GetComponent<HarpoonDraggable>() == null) draggable = null;
+            if (other != null) draggable = other.gameObject;
+            if (draggable != null)
+            {
+                if (draggable.GetComponent<HarpoonDraggable>() != null)
+                {
+                    float angle = GameObject.Find("harpoon").transform.rotation.eulerAngles.z;
+                    draggable.GetComponent<HarpoonDraggable>().rotate(harpoonZRotation + angle);
+                } else
+                {
+                    draggable = null;
+                }
 
-            float angle = GameObject.Find("harpoon").transform.rotation.eulerAngles.z;
-            if (draggable != null) draggable.GetComponent<HarpoonDraggable>().rotate(harpoonZRotation+angle);
+                
+            }
+            
         } else
         {
             obtain = false;
@@ -69,24 +87,28 @@ public class Projectile : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if(fired) {
-            transform.Translate(speed * Vector3.right * Time.deltaTime);
+        float travelDistance = speed * Time.deltaTime;
+        if (fired) {
+            if (maxFireDistance - getDistanceToCanon() < travelDistance) travelDistance = maxFireDistance - getDistanceToCanon();
+            if (travelDistance<=1)
+            {
+                OnTriggerEnter2D(null); //handle stop of harpoon as if an item has been hit
+            }
+            transform.Translate(Vector3.right * travelDistance);
             rope.GetComponent<Rope>().length = (transform.position-initial).magnitude;
-        }
-
-        if (obtain)
+        } 
+        else if (obtain)
         {
-            float lenPull = speed * Time.deltaTime;
+            if (travelDistance > pullDistance) travelDistance = pullDistance;
+            pullDistance -= travelDistance;
 
-            if (lenPull > pullDistance) lenPull = pullDistance;
-            pullDistance -= lenPull;
-            
-            transform.Translate(lenPull * (-1 * Vector3.right));
+            transform.Translate(travelDistance * (-1 * Vector3.right));
             float distPull = getDistanceToCanon(); ; //calculate remaining distance 
             rope.GetComponent<Rope>().length = distPull;
 
-            if (draggable != null) draggable.GetComponent<HarpoonDraggable>().moveDraggable(lenPull);
+            if (draggable != null) draggable.GetComponent<HarpoonDraggable>().moveDraggable(travelDistance);
         }
+
     }
 
     /// <summary>
