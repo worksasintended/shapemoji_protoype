122dacae4a9eb2c4fd76f9bd6a0cc7a0227a1163
Max Schwerdt <MaxSchwerdt@web.de>
2020-06-18 08:34:46 +0200

    118 Merged Master and refactored

--- Shapemoji/Assets/Scripts/InventorySystem.cs
+++ Shapemoji/Assets/Scripts/InventorySystem.cs
@@ -6,21 +6,15 @@ public class InventorySystem : MonoBehaviour
 {
     [SerializeField] public GameObject[] ownedStones;
     [SerializeField] public GameObject[] inventarSlots;
-    [SerializeField] public GameObject inventar;
     
     private void Start()
     {
         foreach (var slot in inventarSlots)
         {
-            // GameObject parent = Instantiate(slot, slot.transform.position, Quaternion.identity, inventar.transform);
             var position = slot.transform.position;
             var layer = slot.layer + 1;
             var index = UnityEngine.Random.Range(0, ownedStones.Length);
-            // GameObject stone = Instantiate(ownedStones[index], parent.transform.position, Quaternion.identity, parent.transform);
             GameObject stone = Instantiate(ownedStones[index], position, Quaternion.identity, slot.transform);
-            // GameObject stone = Instantiate(ownedStones[index], position, Quaternion.identity);
-            // stone.transform.parent = slot.transform;
-            // GameObject stone = PrefabUtility.InstantiatePrefab(ownedStones[index]) as GameObject;
             stone.layer = layer;
         }
     }
