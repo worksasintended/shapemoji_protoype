99dd4d58860a8467d0816fb76a7f08bfcafc1a58
andreas8191522 <66324791+andreas8191522@users.noreply.github.com>
2020-06-18 14:17:23 +0200

    113 harpune funktionalitaet (#7)
    * created HarpuneDemo scene and set MainCamera
    
    * added Harpoon Events
    
    * removed unused files
    
    * added harpoon fire
    
    * changed scene definition and event handling
    
    * added collider events
    
    * reshaped collider
    
    * updated files
    
    * improved functionality
    
    * project restructure
    
    * improved Rope class
    
    * deleted unused files

---
+++ Shapemoji/Assets/Scripts/Canon.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Canon Class with Event Methods
+/// </summary>
+/// <param name="aimDisabled">
+/// Disables Drag & Drop Aim
+/// </param>
+/// <param name="projectile">
+/// Projectile linked to Canon
+/// </param>
+/// <param name="harpoon">
+/// Harpoon linked to Canon
+/// </param>
+public class Canon : MonoBehaviour
+{
+    public bool aimDisabled = false;
+
+    public GameObject projectile;
+    
+    public GameObject harpoon;
+    private bool onDrag = false;
+
+    private float previousAngle = 0;
+    private float nextAngle = 0;    
+
+    /// <summary>
+    /// Start Method
+    /// </summary>
+    void Start()
+    { 
+
+    }
+
+    /// <summary>
+    /// Harpoon Aim using Drag & Drop
+    /// </summary>
+    void OnMouseDrag() {
+        if(!aimDisabled) {
+            Vector3 pos = transform.position;
+            Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            nextAngle = Vector3.SignedAngle(direction-pos, Vector3.up, Vector3.back);
+            if(!onDrag) {
+                previousAngle = nextAngle;
+                onDrag = true;            
+            } else {            
+                harpoon.transform.Rotate(Vector3.forward,nextAngle-previousAngle);
+                previousAngle = nextAngle;
+            }
+        }      
+    }
+
+    /// <summary>
+    /// Sets projectile into fired state
+    /// </summary>
+    void OnMouseUp()
+    {
+        if(!aimDisabled) {
+            onDrag = false;
+            aimDisabled = true;
+            projectile.GetComponent<Projectile>().fired = true;
+        }
+    }
+
+    /// <summary>
+    /// Update method is called once per Frame
+    /// </summary>
+    void Update()
+    {
+            
+    }
+}
---
+++ Shapemoji/Assets/Scripts/Projectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Projectile Class with Event Handlers as Methods
+/// </summary>
+/// <param name="fired">
+/// Set true in order to fire the Projectile
+/// </param>
+/// <param name="speed">
+/// Speed of the fired Projectile
+/// </param>
+/// <param name="rope">
+/// Rope linked to Projectile
+/// </param>
+public class Projectile : MonoBehaviour
+{
+    public bool fired = false;
+    public float speed = 400;
+    public GameObject rope;
+    private Vector3 initial;
+
+    /// <summary>
+    /// Method to set initial projectile position
+    /// </summary>
+    void Start()
+    {
+        initial = transform.position;
+    }
+
+    /// <summary>
+    /// Stop movement on collision
+    /// </summary>
+    void OnTriggerEnter2D(Collider2D other) {       
+        fired = false;
+    }
+
+    /// <summary>
+    /// Updates projectile coordinates
+    /// </summary>
+    void Update()
+    {
+        if(fired) {
+            transform.Translate(speed * Vector3.right * Time.deltaTime);
+            rope.GetComponent<Rope>().length = (transform.position-initial).magnitude;
+        }
+    }
+}
