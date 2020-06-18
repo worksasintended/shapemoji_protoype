6dd7ef25ab2a438e453b2ff92a51a44d6dbe218d
kevin.buecher <kevin.buecher@hochschule-stralsund.de>
2020-06-18 22:29:37 +0200

    add drag drop functionality for sprite renderer + add checker for correct position

--- Shapemoji/Assets/Scripts/DragAndDrop.cs
+++ Shapemoji/Assets/Scripts/DragAndDrop.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
 
     private bool isDragging;
+    [SerializeField] private SpriteRenderer werkbank;
+    [SerializeField] private SpriteRenderer emoji;
+    private Vector2 inventoryPos;
+    private Vector2 lastPos;
 
+    void Awake()
+    {
+        inventoryPos = GetComponent<RectTransform>().anchoredPosition;
+        lastPos = GetComponent<RectTransform>().anchoredPosition;
+    }
+    
     public void OnMouseDown()
     {
         isDragging = true;
@@ -19,10 +30,33 @@ public class DragAndDrop : MonoBehaviour
 
     void Update()
     {
+        RectTransform itemBorder = GetComponent<RectTransform>();
+        RectTransform werkbankBorders = werkbank.GetComponent<RectTransform>();
+        RectTransform emojiBorders = emoji.GetComponent<RectTransform>();
+        
         if (isDragging)
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(mousePosition);
+
+        }else if(!rectOverlaps(itemBorder, werkbankBorders)){
+            GetComponent<RectTransform>().anchoredPosition = inventoryPos;
+        
+        }else if(rectOverlaps(itemBorder, werkbankBorders))
+        {
+            GetComponent<RectTransform>().anchoredPosition = werkbank.GetComponent<RectTransform>().anchoredPosition;
+            lastPos = GetComponent<RectTransform>().anchoredPosition;
+        }else if (rectOverlaps(itemBorder, emojiBorders) && lastPos == werkbank.GetComponent<RectTransform>().anchoredPosition)
+        {
+            GetComponent<RectTransform>().anchoredPosition = emoji.GetComponent<RectTransform>().anchoredPosition;
         }
+    }
+    
+    bool rectOverlaps(RectTransform rectTrans1, RectTransform rectTrans2)
+    {
+        Rect rect1 = new Rect(rectTrans1.localPosition.x, rectTrans1.localPosition.y, rectTrans1.rect.width, rectTrans1.rect.height);
+        Rect rect2 = new Rect(rectTrans2.offsetMin.x, rectTrans2.offsetMin.y, rectTrans2.rect.width, rectTrans2.rect.height);
+
+        return rect2.Overlaps(rect1);
     }
 }
