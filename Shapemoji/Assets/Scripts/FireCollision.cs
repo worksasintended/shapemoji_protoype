92af88b02e7c608ecf289a48a2d602879c1fb8af
worksasintended <marc@marschalls.net>
2020-06-20 21:57:09 +0200

    added files

---
+++ Shapemoji/Assets/Scripts/FireCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FireCollision : MonoBehaviour
+{
+    private bool isDragging;
+    
+
+    public void OnMouseDown(){
+        isDragging = true;
+    }
+
+    public void OnMouseUp(){
+        isDragging = false;
+    }
+
+    void Update(){
+        if(isDragging){
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D fireObject){
+        fireObject.gameObject.SetActive(false);
+    }
+
+    
+}
